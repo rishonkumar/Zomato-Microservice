0800443a39efe9346d83fd489a027c0a1be3723e
rishonkumar <rishon.kumar.rahi@gmail.com>
2026-02-13 21:48:36 +0530

    Implemented User Role api and User authentication middleware

--- services/auth/src/routes/Auth.ts
+++ services/auth/src/routes/Auth.ts
@@ -1,9 +1,12 @@
 import express from "express"
-import { loginUser } from "../controller/auth.js"
+import { addUserRole, loginUser } from "../controller/auth.js"
+import { isAuth } from "../middlewares/isAuth.js"
 
 const router = express.Router()
 
 router.post("/login", loginUser)
+
+router.put("/add/role", isAuth, addUserRole)
 
 
 export default router
