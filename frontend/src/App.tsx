01fab062560924e2f91ca9af0adade4cda91745a
rishonkumar <rishon.kumar.rahi@gmail.com>
2026-02-19 19:39:56 +0530

    Created Login Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/login"
+import { Toaster } from "react-hot-toast"
 
 const App = () => {
   return (
-    <div className="text-red-500">App</div>
+    <div className="text-red-500">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Toaster />
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,58 @@
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { authService } from "../main"
+import toast from "react-hot-toast"
+import { useGoogleLogin } from '@react-oauth/google';
+import { FcGoogle } from "react-icons/fc"
+
+
+const Login = () => {
+
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+
+    const responsGoogle = async (authResult: any) => {
+        setLoading(true)
+        try {
+            const res = await axios.post(`${authService}/api/v1/auth/login`, {
+                code: authResult['code']
+            })
+
+            localStorage.setItem("token", res.data.token)
+            toast.success(res.data.message)
+            setLoading(false)
+            navigate("/")
+        } catch (err) {
+            console.log(err)
+            toast.error("Proble while login")
+            setLoading(false)
+        }
+    }
+
+    const googleLogin = useGoogleLogin({
+        onSuccess: responsGoogle,
+        onError: responsGoogle,
+        flow: "auth-code"
+    })
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-white px-4">
+            <div className="w-full max-w-sm space-y-6">
+                <h1 className="text-center text-3xl font-bold text-[#E23774]">Zomatato</h1>
+
+                <p className="text-center text-sm text-gray-500">Log in or sign up to continue</p>
+
+                <button onClick={googleLogin} disabled={loading}
+                    className="flex w-full items-center justify-center gap-3 rounded-xl border border-gray-300 bg-white px-4 py-3"><FcGoogle size={20} />{loading ? "Sigining in ..." : "Contiune with google"}</button>
+
+                <p className="text-center text-xs text-gray-500">
+                    By Continuing you agree with our {" "} <span className="text-[#E23774]">Terms of Service</span> & {" "} <span className="text-[#E23774]">Privacy Policy </span>
+                </p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Login
